0af08045db0f63bdd15d93042af1428616f9b2d3
ula <ulasmyczynska@gmail.com>
2022-09-11 15:38:00 +0200

    Fix installation file for feature selection

--- packages_installation/at_home/feature_selection/R_packages_install.R
+++ packages_installation/at_home/feature_selection/R_packages_install.R
@@ -4,3 +4,8 @@ packageWithVersionList = list(
   c('Boruta', '7.0.0'),
   c('praznik', '11.0.0')
 )
+
+
+for (packageNameVersion in packageWithVersionList) {
+  install_version(packageNameVersion[1], version = packageNameVersion[2], dependencies = TRUE, upgrade = 'default');
+}
